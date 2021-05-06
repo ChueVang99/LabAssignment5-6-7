b0d14adba17800f4b508083066c8200d3e62e332
Chue Vang <chuevang10@gmail.com>
2021-05-06 15:09:03 -0700

    push day one rest api

---
+++ dist/seed/user.seed.js
@@ -0,0 +1,11 @@
+[{
+  "id": 1,
+  "email": "chuevang10@gmail.com",
+  "username": "chue",
+  "password": "545313",
+  "super_admin": true,
+  "site_admin": true,
+  "hash": "CVI2C3WKYEdbL4yi3B9eE",
+  "is_activated": true,
+  "active": true
+}]
